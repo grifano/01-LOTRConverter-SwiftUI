df8b9ca060a12d809aed0bd975bfaef0a629156a
Serhii Orlenko <sorlenk@gmail.com>
2026-06-12 22:11:51 +0200

    Initial Commit

---
+++ 01 LOTR Converter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  01 LOTR Converter
+//
+//  Created by sorlenko on 12/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 01 LOTR Converter/_1_LOTR_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_LOTR_ConverterApp.swift
+//  01 LOTR Converter
+//
+//  Created by sorlenko on 12/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct _1_LOTR_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
