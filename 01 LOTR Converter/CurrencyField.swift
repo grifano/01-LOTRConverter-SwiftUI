6b93b4cb96d7d01534e793edd82b43aa531c9ca9
Serhii Orlenko <sorlenk@gmail.com>
2026-06-26 14:06:18 +0200

    Coding Challenges: - Refactor is completed

---
+++ 01 LOTR Converter/CurrencyField.swift
@@ -0,0 +1,18 @@
+//
+//  CurrencyField.swift
+//  01 LOTR Converter
+//
+//  Created by sorlenko on 25/06/2026.
+//
+
+import SwiftUI
+
+struct CurrencyField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CurrencyField()
+}
